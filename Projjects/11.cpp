369eb75070b831e1f199511fbcac58499db7deee
Ayush PANDAY <AyushPanday@developer12345.onmicrosoft.com>
2022-11-27 17:51:18 +0530

    11 done

---
+++ Projjects/11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	long long int
+		number,
+		number1 = -1,
+		number2 = 1,
+		sum;
+	cout << "Enter the number -> ";
+	cin >> number;
+	for (int i = 0; i < number; i++)
+	{
+		sum = number1 + number2;
+		number1 = number2;
+		number2 = sum;
+		cout << sum << endl;
+	}
+//  getch();
+	return 0;
+}
